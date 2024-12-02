2108e4c13c88bba21ab445f27ec46ead34a0c623
Zufarzafar1 <zabdurakhmonov2@students.wiut.uz>
2024-12-02 12:28:41 +0500

    intermediate

--- event-manager-api/Program.cs
+++ event-manager-api/Program.cs
@@ -34,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
--- event-manager-api/obj/Debug/net6.0/event-manager-api.AssemblyInfo.cs
+++ event-manager-api/obj/Debug/net6.0/event-manager-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("event-manager-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4e6a931545e201193c644ad852837531d0d29f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8824dd7718df69b366db807068ad8acaf9c587b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("event-manager-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("event-manager-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
